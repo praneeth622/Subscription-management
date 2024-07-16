af6a7abda53dd2c970d3fe937c59850cbdd6974a
jashwanth-randomwalk <jashwanth.p@randomwalk.ai>
2024-07-16 23:29:19 +0530

    added popup

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -5,6 +5,7 @@ import { useUser } from "@clerk/clerk-react";
 import { ToastContainer, toast } from 'react-toastify';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { Popup } from "@/components/component/popup";
 
 
 const Dashboard: React.FC = () => {
@@ -46,7 +47,7 @@ const Dashboard: React.FC = () => {
                 <h1 className="text-4xl font-bold mt-5">Welcome, {user?.firstName}</h1>
                 <div className="bg-customPurple p-6 rounded-lg h-[40%] w-[90%] mt-[20px]">
                   <h1> Welcome to Artistry</h1>
-                   
+                   <Popup/>
                   <button 
       className="bg-buttonPurple hover:bg-darkP text-white font-bold py-2 px-4 rounded"
       onClick={() => navigate('/create')}
